fede902e04f601c3d5a955016d7786a1ba8ceaa6
HarryWeasley3 <3556762074g@gmail.com>
2023-09-10 12:29:38 +0800

    third commit

--- 霍尔效应/draw_B-X.py
+++ 霍尔效应/draw_B-X.py
@@ -8,17 +8,18 @@ IH = 5.00   # mA
 
 
 U1 = [-2.89, -4.24, -4.88, -5.17, -5.38, -5.46, -5.50, -5.50, -5.50, -5.46, -5.36, -5.07, -4.72, -3.84, -2.18]
-U2 = [4.04, 5.43, 6.11, 6.40, 6.62, 6.70, 6.74, 6.74, 6.7,6.70,6.61, 6.32, 5.94, 5.06, 3.45]
+U2 = [4.04, 5.43, 6.11, 6.40, 6.62, 6.70, 6.74, 6.74, 6.74,6.70,6.61, 6.32, 5.94, 5.06, 3.45]
 U3 = [-3.46, -5.10, -6.14, -6.43, -6.63, -6.71, -6.74, -6.74, -6.74, -6.70, -6.61, -6.32, -5.97, -5.08, -3.46]
 U4 = [2.86, 4.20, 4.87, 5.15, 5.36, 5.44, 5.48, 5.48, 5.48, 5.45, 5.35, 5.06, 4.69, 3.81, 2.21]
 
 
-U = list(map(lambda a,b,c,d:a-b+c-d, U1,U2,U3,U4))
+U = list(map(lambda a,b,c,d:(a-b+c-d)/4, U1,U2,U3,U4))
 
-if U[8]<0:
-    U = [-x for x in U]
+U = [-x for x in U if x<0]
+print(U)
 
 
+plt.style.use("seaborn")
 plt.figure(figsize = (20,12))
 
 plt.xlabel("x/cm", fontsize=16)
@@ -28,16 +29,31 @@ plt.ylabel("B/T", fontsize=16)
 gap = np.linspace(2,28,26000)
 
 B = [x/(KH*IH) for x in U]
+print(B)
 B_value = np.array(B)
 x_value = np.array([2,3,4,5,7,9,12,15,18,21,23,25,26,27,28])
+
+B_min = [0.007053*0.99 for _ in B]
+
 
 model = make_interp_spline(x_value,B_value)
 line = model(gap)
 
 
 plt.plot(gap, line)
+plt.scatter(x_value,B_value)
+plt.plot(x_value,B_min)
 plt.xticks(np.arange(1,29,1), fontsize=16)
-plt.yticks(np.arange(0.014,0.032,0.002), fontsize=16)
+plt.yticks(np.arange(0.0030,0.0074,0.0002), fontsize=16)
+
+tolerance = 1e-7  # 定义容差值
+
+for index, value in enumerate(line):
+    if abs(value - B_min[0]) < tolerance:
+        intersection = gap[index]
+        print("Intersection:", intersection)
+
+        
 plt.savefig("./B-X.png")
 plt.show()
 
--- 霍尔效应/draw_Kh.py
+++ 霍尔效应/draw_Kh.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import make_interp_spline
+from math import *
 
-
+L = 0.26 # m
+N = 3000
+D = 0.035 # m
+Ih = (5.00)*(10**(-3)) # A
+u = 4*np.pi*(10**(-7))
 
 U1 = [0.60, -0.60, -1.85, -3.08, -4.30, -5.53, -6.76, -7.97, -9.21, -10.43, -11.66]
 U2 = [-0.61, 0.60, 1.82, 3.05, 4.28, 5.51, 6.74, 7.97, 9.20, 10.43, 11.65]
@@ -10,13 +15,15 @@ U3 = [-0.61, -1.85, -3.08, -4.30, -5.54, -6.77, -7.99, -9.22, -10.45, -11.67, -1
 U4 = [-0.62, 0.58, 1.81, 3.03, 4.26, 5.49, 6.71, 7.95, 9.18, 10.42, 11.64]
 
 U = list(map(lambda a,b,c,d: (a-b+c-d)/4, U1,U2,U3,U4))
+U = [abs(x) for x in U]
 print(U)
 
-if U[8]<0:
-    U = [-x for x in U]
+plt.style.use("seaborn")
 
-plt.xlabel("Im")
-plt.ylabel("U")
+plt.figure(figsize=(15,9))
+
+plt.xlabel("Im",fontsize=20)
+plt.ylabel("Uh",fontsize=20)
 
 gap = np.linspace(0,1000,10000)
 u_value = np.array(U)
@@ -25,9 +32,19 @@ Im_value = np.array(range(0,1100,100))
 model = make_interp_spline(Im_value,u_value)
 line = model(gap)
 
+# plt.plot(u_value,Im_value)
+plt.xticks(np.arange(0,1100,50),fontsize=16)
+plt.yticks(np.arange(0,13,0.5),fontsize=16)
 plt.plot(gap,line)
+plt.scatter(Im_value,u_value)
 # plt.show()
 plt.savefig("./U-Im.png")
+
+K = (U[10] - U[1])/900
+print("K=",K)
+Kh = (sqrt((L**2)+(D**2))/(u*N*Ih))*K
+print("Kh=",Kh)
 plt.show()
+
 
 
